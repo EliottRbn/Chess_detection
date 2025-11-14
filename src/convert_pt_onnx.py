461ef297a794eb4b79c4649ca17c1379e86b14da
Quentin du Ranquet <quentin.chardon-du-ranquet@ipsa.fr>
2025-11-14 09:28:08 +0100

    Update

--- src/convert_pt_onnx.py
+++ src/convert_pt_onnx.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
 
 model = YOLO("src/runs/best.pt")
-model.export(format="tfjs",optimize=True,device="cpu")
-print("Model exported to TensorFlow.js format.")
+model.export(format="onnx",optimize=True)
+print("Model exported to ONNX format.")
